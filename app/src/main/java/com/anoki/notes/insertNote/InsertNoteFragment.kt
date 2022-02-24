6b397a98691308e4055c71ccc8627cdf0f30e850
Simone Rosani <rsn.smn@gmail.com>
2022-02-24 11:45:40 +0100

    fixed addNote in fragment

--- app/src/main/java/com/anoki/notes/insertNote/InsertNoteFragment.kt
+++ app/src/main/java/com/anoki/notes/insertNote/InsertNoteFragment.kt
@@ -10,6 +10,7 @@ import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import com.anoki.notes.NotesViewModel
+import com.anoki.notes.data.Note
 import com.anoki.notes.databinding.FragmentInsertNoteBinding
 import com.anoki.notes.databinding.FragmentNotesListBinding
 
@@ -36,7 +37,7 @@ class InsertNoteFragment : Fragment() {
         val noteTitle = binding.etTitle.text.toString()
         val noteBody = binding.etBody.text.toString()
         if (noteTitle.isNotEmpty() && noteBody.isNotEmpty()) {
-            viewModel.addNote(noteTitle, noteBody)
+            viewModel.addNote(Note(null, noteTitle, noteBody))
         } else {
             Toast.makeText(
                 this.context,
